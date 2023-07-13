8a7aed24a49692bfda2947d0ba485145f7bd1df2
RennieChien <133186945+RennieChien@users.noreply.github.com>
2023-07-13 14:29:38 +0800

    First commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xF2D2BD, 0xAAAAAA, 0.8);
+		const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -74,7 +74,7 @@ class App{
         
         const self = this;
         
-        loader.load( './assets/hdr/night_free_Ref.hdr', ( texture ) => {
+        loader.load( './assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
           const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
           pmremGenerator.dispose();
 
@@ -238,7 +238,7 @@ class App{
         if (this.proxy === undefined) return;
         
         const wallLimit = 1.3;
-        const speed = 1.5;
+        const speed = 2;
 		let pos = this.dolly.position.clone();
         pos.y += 1;
         
